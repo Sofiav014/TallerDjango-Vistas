13916b61d99359d8eeac4ca7ca6c081fa5760e5e
Sofia Velasquez <s.velasquezm2@uniandes.edu.co>
2023-02-10 11:42:50 -0500

    BD

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "<nombrebd>",
-        "USER": "<usuario>",
-        "PASSWORD": "<clave>",
+        "NAME": "svmbd",
+        "USER": "sofiv014",
+        "PASSWORD": "LULU123",
         "HOST": "localhost",
         "PORT": "",
     }
